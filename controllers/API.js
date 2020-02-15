283d8b4e0b95fa7ed2f69b476bf9e4b0ef6b4462
mahadbajwa97 <52116081+mahadbajwa97@users.noreply.github.com>
2020-02-15 17:46:10 +0500

    Update API.js

--- controllers/API.js
+++ controllers/API.js
@@ -64,7 +64,7 @@ exports.Login = function (req, res, User) {
                     console.log("data");
                     httpMsgs.show500(req, res, err);
                 }
-                else if (data.isValidated) {
+                else if (data[0].isValidated) {
                     let hash = crypto.createHash('md5').update(User.password).digest("hex")
 
                     db.executeSql("exec Login @userName='" + User.userName + "', @emailAddress ='', @password ='" + hash + "'", function (data, err) {
@@ -81,7 +81,7 @@ exports.Login = function (req, res, User) {
                     console.log(data);
                     httpMsgs.sendJson(req, res, data);
                 }
-                else if (!data.isValidated) {
+                else if (!data[0].isValidated) {
                     res.json({ "status": data });
                     console.log("The email is not verified")
                 }
