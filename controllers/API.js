db0d0b8e517522aca46741955e17d711543f49a1
mahadbajwa97 <52116081+mahadbajwa97@users.noreply.github.com>
2020-02-15 20:28:27 +0500

    Update API.js

--- controllers/API.js
+++ controllers/API.js
@@ -64,7 +64,7 @@ exports.Login = function (req, res, User) {
                     let hash = crypto.createHash('md5').update(User.password).digest("hex");
                     
                     db.executeSql("exec login @userName='" + User.userName + "', @password ='" + hash + "'", function (data, err) {
-                        res.send(data);
+                        
                         if (err) 
                         {
                             console.log(3);
