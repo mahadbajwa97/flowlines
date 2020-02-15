418a71c8bd61066ec3b5c72e8488f50a48d85af5
mahadbajwa97 <52116081+mahadbajwa97@users.noreply.github.com>
2020-02-15 20:07:31 +0500

    Update API.js

--- controllers/API.js
+++ controllers/API.js
@@ -62,7 +62,7 @@ exports.Login = function (req, res, User) {
                 
                  
                     let hash = crypto.createHash('md5').update(User.password).digest("hex");
-                    res.send(JSON.parse(User));
+                    
                     db.executeSql("exec Login @userName='" + User.userName + "', @emailAddress ='', @password ='" + hash + "'", function (logdata, err) {
                         res.send(data);
                         if (err) 
@@ -73,6 +73,7 @@ exports.Login = function (req, res, User) {
                         }
                         else if(data[0].isValidated) {
                             console.log(logdata);
+                            res.send(data+"I am here");
                             httpMsgs.sendJson(req, res, logdata);
                         }
                         
