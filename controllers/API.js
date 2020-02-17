0b346cab7208dfe3de12cf3caee68c2567bcb74e
mahadbajwa97 <52116081+mahadbajwa97@users.noreply.github.com>
2020-02-17 15:27:59 +0500

    Update API.js

--- controllers/API.js
+++ controllers/API.js
@@ -5,7 +5,19 @@ var randomstring = require("randomstring");
 const mailer = require("./mailer");
 const crypto = require('crypto');
 exports.getProfileView = function (req, res, ID) {
-    db.executeSql("exec profile_view @userID="+ID, function (data, err) {
+    db.executeSql("exec UserFeed @userID="+ID, function (data, err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.json(data);
+        }
+
+
+    });
+}
+exports.getUserfeed = function (req, res, ID) {
+    db.executeSql("exec UserFeed @userID="+ID, function (data, err) {
         if (err) {
             console.log(err);
         }
