9f8dcffe185e1f5098933ac46a7feb17aa515716
mahadbajwa97 <52116081+mahadbajwa97@users.noreply.github.com>
2020-02-17 22:38:45 +0500

    Update API.js

--- controllers/API.js
+++ controllers/API.js
@@ -5,7 +5,7 @@ var randomstring = require("randomstring");
 const mailer = require("./mailer");
 const crypto = require('crypto');
 exports.getProfileView = function (req, res, ID, currentID) {
-    db.executeSql("exec Profile_view @userID="+ID+",@currentUserID="+currentID, function (data, err) 
+    db.executeSql("exec Profile_view @userID="+ID+",@currentUserID="+currentID+",@followRequest=0, @following=0", function (data, err) 
     {
         if (err) {
             console.log(err);
