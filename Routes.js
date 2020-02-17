8f773e5cd15fd331c506e1024a3fc6553b3e3a88
mahadbajwa97 <52116081+mahadbajwa97@users.noreply.github.com>
2020-02-18 01:56:31 +0500

    Update Routes.js

--- Routes.js
+++ Routes.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports = function (app) {
     var exp = require("./controllers/API");
-
+    app.route('/privacyPolicy').get(function (req, res) {
+        res.render("./privacy_policy");
+    });
     app.route('/profileView/:currentID/:id').get(function (req, res) {
         exp.getProfileView(req, res, req.params.id, req.params.currentID);
     });
