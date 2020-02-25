97e14ab4e7ca1b7432416703ded5e12df481ce1e
mahadbajwa97 <52116081+mahadbajwa97@users.noreply.github.com>
2020-02-25 15:16:29 +0500

    Update API.js

--- controllers/API.js
+++ controllers/API.js
@@ -87,7 +87,7 @@ exports.Registers = function (req, res, User) {
                     });
                     //Create New Email
                     //mailer.Mail(User.fullName, User.emailAddress, SecretToken);
-                    setTimeout(function () {mailer.Mail_1(User.emailAddress, User.fullName, SecretToken);}, 2000);
+                    setInterval(function () {mailer.Mail_1(User.emailAddress, User.fullName, SecretToken);}, 2000);
                     
                 }
             });
