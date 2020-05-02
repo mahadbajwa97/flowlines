7037962f9d7d0cfba873d9d0faf2aac13ac60def
mahadbajwa97 <52116081+mahadbajwa97@users.noreply.github.com>
2020-05-02 20:54:49 +0500

    Update mailer.js

--- controllers/mailer.js
+++ controllers/mailer.js
@@ -4,7 +4,7 @@ var Mailgun = require('mailgun-js');
 var sendgrid = require('sendgrid')('azure_3d95a80d87700eb5234ed1af36865832@azure.com', 'QWErtyuiop46');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey('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');
-const mailjet = require('node-mailjet').connect('419ddf082de99b9a566f2586d398680d', '7fab9a64b6174a11c81ee44773d6dc4b');
+const mailjet = require('node-mailjet').connect('b24873ba358a2ec15f567ca58577d4ff', '7c689b7374a450a32d1fab8a63ad38b1');
 
 exports.Mail_1 = function (to_, fullName, code) {
 
